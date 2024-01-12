a0c77b1306d27220e007c991941a4c6811a3aa2f
siph <dawkins.chris.dev@gmail.com>
2024-01-12 13:55:46 -0700

    better derivation

--- flake.nix
+++ flake.nix
@@ -14,19 +14,18 @@
     flake-utils.lib.eachDefaultSystem (system: let
       pkgs = nixpkgs.legacyPackages.${system};
     in {
-      packages = {
-        default = pkgs.stdenvNoCC.mkDerivation rec {
+      packages = with pkgs; {
+        default = stdenvNoCC.mkDerivation rec {
           name = "quick-mark";
           src = ./.;
+          nativeBuildInputs = [makeBinaryWrapper];
           buildInputs = with pkgs; [nushell makeBinaryWrapper ffmpeg];
           installPhase = ''
-            mkdir -p $out/nushell
             mkdir -p $out/bin
-
-            cp ./${name}.nu $out/nushell/${name}.nu
-
-            makeBinaryWrapper ${pkgs.nushell}/bin/nu $out/bin/${name} \
-              --add-flags $out/nushell/${name}.nu
+            mkdir -p $out/share/${name}
+            cp ./${name}.nu $out/share/${name}
+            makeWrapper ${nushell}/bin/nu $out/bin/${name} \
+              --add-flags "$out/share/${name}/${name}.nu"
           '';
         };
       };
